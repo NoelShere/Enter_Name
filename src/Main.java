aed157cbed3cfa90b9e0df674d1549da2ddb2332
noelshere <noelshere60@gmail.com>
2023-10-11 10:41:37 -0400

    added changes

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+        String Name, Address; // Vars
+
+        // Name
+
+        System.out.println("Enter your name: ");
+
+        Scanner s = new Scanner(System.in);
+
+        Name = s.nextLine();
+
+
+        // Address
+
+        System.out.println("Enter your Address: ");
+
+        Scanner a = new Scanner(System.in);
+
+        Address = a.nextLine();
+
+
+        // Print Method
+
+        print_result(Name,Address);
+
+
+    }
+
+    public static void print_result(String n, String a){
+
+        System.out.println("Name: " + n);
+        System.out.println("Address: " + a);
+
+
+    }
+
+}
